8d49606c51c21382f5117213a82f1ec550149d0f
Ellen Nickles <enickles@gmail.com>
2023-03-04 18:50:59 -0500

    start to example

--- sketch.js
+++ sketch.js
@@ -5,9 +5,8 @@ function setup() {
 
 function draw() {
   background(250, 250, 0);
-
   fill(0, 150, 220);
-  textSize(sin(frameCount/10) * 50);
+  textSize(width/8);
   textAlign(CENTER);
   text("Spring break 🎉", width/2, height/2);
 }
