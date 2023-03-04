f18f8e03a998cf9ce8f62fa56c94325299f3dd0d
Ellen Nickles <enickles@gmail.com>
2023-03-04 14:42:09 -0500

    animate string

--- sketch.js
+++ sketch.js
@@ -1,3 +1,4 @@
+
 function setup() {
   createCanvas(400, 400);
 }
@@ -6,7 +7,7 @@ function draw() {
   background(250, 250, 0);
 
   fill(0, 150, 220);
-  textSize(width/8);
+  textSize(sin(frameCount/10) * 50);
   textAlign(CENTER);
   text("Spring break 🎉", width/2, height/2);
 }
